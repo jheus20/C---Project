8c01af9b14caeed5ee8be9d9bbe7c8ff2cfbeecb
jheus20 <79778834+jheus20@users.noreply.github.com>
2025-07-19 15:56:47 +0800

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string favfood;
+   
+    int favnumber;
+    
+    string favcolor;
+
+    string hidden;
+
+    cout << "what is jijo favorite food?: ";
+    cin >> favfood;
+
+    cout << "what is jijo favorite number?: ";
+    cin >> favnumber;
+
+    cout << "what is jijo favorite color?: ";
+    cin >> favcolor;
+
+    cout << "what is jijo hidden talent?: ";
+    cin >> hidden;
+
+    cout << "jijo's favorite food is " << favfood << ", his favorite number is " 
+         << favnumber << ", his favorite color is " << favcolor 
+         << ", and his hidden talent is " << hidden << "." << endl;
+    
+
+    cout << " i love you so much ahu, pasankyu sa pag answer hihih. Goodnight and have a good sleep uyy!! <3 " << endl;
+
+    return 0;
+}
