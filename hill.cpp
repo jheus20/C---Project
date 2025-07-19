7a5b7d8c2236c612617565ac5a16d0e585fdea9a
jheus20 <79778834+jheus20@users.noreply.github.com>
2025-07-19 16:20:11 +0800

    Add files via upload

---
+++ hill.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std; 
+
+int main() { 
+
+    int month = 4;
+    
+    switch(month) {
+
+    case 1:
+    cout << "january" << endl;
+    break;
+
+    case 2:
+    cout << "february" << endl;
+    break;
+
+    case 3:
+    cout << "march" << endl;
+    break;
+
+    case 4:
+    cout << "december" << endl;
+    break;
+
+    default:
+    cout << "Wdym?" << endl;
+  
+  }
+
+
+
+    return 0; 
+
+    
+}
